466892a8566722e01754b39f993e72378dd437f3
HarshilPatel007 <p.h.hacker007@gmail.com>
2022-05-14 19:20:55 +0530

    get selected model fields

--- src/models_parser.js
+++ src/models_parser.js
@@ -13,24 +13,37 @@ function modelsParser() {
                     let selectedDirPath = clipboardText
                     let modelsFilePath = path.join(selectedDirPath, 'models.py')
                     let pathUri = vscode.Uri.file(modelsFilePath)
+                    let exeCmd = vscode.commands.executeCommand('vscode.executeDocumentSymbolProvider', pathUri)
+
+                    let modelClass = []
+                    let modelFields = []
 
                     if (fs.existsSync(modelsFilePath)) {
-                        vscode.commands.executeCommand('vscode.executeDocumentSymbolProvider', pathUri)
-                            .then((result) => {
-                                result.forEach(async elements => {
-                                    if (elements.kind === 4) {
-                                        let fields = elements.children.filter(f => f.kind === 12)
-                                        await Promise.all(fields.map(async (f) => {
-                                            let fieldsName = await f.name
-                                            console.log(fieldsName)
-                                        }))
+                        exeCmd.then((result) => {
+                            result.forEach((elements) => {
+                                if (elements.kind === 4) {
+                                    modelClass.push(elements.name)
+                                    // console.log(elements.children)
+                                    let fields = elements.children.filter(f => f.kind === 12)
+                                    fields.forEach((f) => {
+                                        let classFields = elements.name + ':' + f.name
+                                        modelFields.push(classFields)
+                                    })
+                                }
+                            })
+                            vscode.window.showQuickPick(modelClass).then((selectedCls) => {
+                                modelFields.forEach(fc => {
+                                    if (fc.includes(selectedCls) === true) {
+                                        let cls = selectedCls + ':'
+                                        let fields = fc.replace(cls, '')
+                                        console.log(fields)
                                     }
                                 })
                             })
+                        })
                     } else {
                         vscode.window.showErrorMessage('models.py not found. select app folder and then execute command.', ...['Ok'])
                     }
-
                 })
             })
     } // get_model_fields
